b6969ca8f4a95e48393ede706eceef2852bc911b
Danil Shamsiev <veismahs@outlook.com>
2023-05-11 04:24:54 +0400

    refactor: better window navigation

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -32,3 +32,10 @@ vim.keymap.set("n", "<leader>o", "o<ESC>cc<CR>")
 
 vim.keymap.set("n", "gg", "ggg0");
 vim.keymap.set("n", "G", "Gg0");
+
+vim.keymap.set("n", "<leader>wh", "<C-w>h");
+vim.keymap.set("n", "<leader>wj", "<C-w>j");
+vim.keymap.set("n", "<leader>wk", "<C-w>k");
+vim.keymap.set("n", "<leader>wl", "<C-w>l");
+vim.keymap.set("n", "<leader>ws", "<C-w>s");
+vim.keymap.set("n", "<leader>wv", "<C-w>v");
