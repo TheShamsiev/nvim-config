085d60d4c08cc855569f804c92ba7db78c649cd2
Danil Shamsiev <veismahs@outlook.com>
2023-05-12 04:37:28 +0400

    refactor: remove hl for StatusLineNC

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,7 +5,6 @@ function SetColor(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "StatusLine", { bg = "#354634" })
-	vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#859684", fg = "#000000" })
 end
 
 SetColor()
