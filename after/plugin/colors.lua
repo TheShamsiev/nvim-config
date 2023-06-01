1fa0bb8ef85d758535a4a15022b87cda33a9bcf8
Danil Shamsiev <veismahs@outlook.com>
2023-06-01 08:39:20 +0400

    refactor: slight colors change

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,7 +4,7 @@ function SetColor(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "StatusLine", { bg = "#354634" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 end
 
 SetColor()
