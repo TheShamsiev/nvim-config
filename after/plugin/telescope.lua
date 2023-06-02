b5adcb68af403c6a8cf292caede94fd3411e3d67
Danil Shamsiev <veismahs@outlook.com>
2023-06-02 06:09:51 +0400

    feat: telescope find selected text

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,3 +2,33 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
+
+
+local keymap = vim.keymap.set
+local tb = require('telescope.builtin')
+local opts = { noremap = true, silent = true }
+
+keymap('n', '<space>g', ':Telescope current_buffer_fuzzy_find<cr>', opts)
+keymap('v', '<space>g', function()
+	local text = vim.getVisualSelection()
+	tb.current_buffer_fuzzy_find({ default_text = text })
+end, opts)
+
+keymap('n', '<space>G', ':Telescope live_grep<cr>', opts)
+keymap('v', '<space>G', function()
+	local text = vim.getVisualSelection()
+	tb.live_grep({ default_text = text })
+end, opts)
