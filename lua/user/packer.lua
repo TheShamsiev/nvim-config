0938f6187b0d5b82fbc3506f3356c0c1935cce82
Danil Shamsiev <veismahs@outlook.com>
2023-06-02 05:05:11 +0400

    feat: add colorizer

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -48,4 +48,5 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+  use("norcalli/nvim-colorizer.lua")
 end)
