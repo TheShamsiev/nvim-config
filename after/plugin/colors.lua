d6f103439b5d84511fd90e76b6a6ab1922aaacc4
Danil Shamsiev <veismahs@outlook.com>
2023-06-02 04:32:13 +0400

    feat: dim non-current windows

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,6 +3,7 @@ function SetColor(color)
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "#151515" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1c1c1c" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#151515" })
   vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#151515" })
   vim.api.nvim_set_hl(0, "StatusLine", { fg = "#1c1c1c", bg = "#1c1c1c" })
