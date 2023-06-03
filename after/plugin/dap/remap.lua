33dea6eff1836601c21c25cf5785ca86a50ae5c0
Danil Shamsiev <veismahs@outlook.com>
2023-06-03 20:47:36 +0400

    refactor: dap and dap-ui setup with bindings

--- after/plugin/dap/remap.lua
+++ after/plugin/dap/remap.lua
@@ -1,7 +1,11 @@
 local dap = require("dap")
 
-vim.keymap.set("n", "\\b", function () dap.toggle_breakpoint() end)
-vim.keymap.set("n", "\\c", function () dap.continue() end)
-vim.keymap.set("n", "\\o", function () dap.step_over() end)
-vim.keymap.set("n", "\\i", function () dap.step_into() end)
-vim.keymap.set("n", "\\r", function () dap.repl.open() end)
+-- Set keymaps to control the debugger
+vim.keymap.set('n', '<F5>', dap.continue)
+vim.keymap.set('n', '<F10>', dap.step_over)
+vim.keymap.set('n', '<F11>', dap.step_into)
+vim.keymap.set('n', '<F12>', dap.step_out)
+vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+vim.keymap.set('n', '<leader>B', function()
+  dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+end)
