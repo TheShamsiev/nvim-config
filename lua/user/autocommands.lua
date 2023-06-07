ccd46cece96c5f93e0883c99f3581ccf654adfa0
Danil Shamsiev <veismahs@outlook.com>
2023-06-07 09:03:47 +0400

    feat: autoupdate file contents on any button press

---
+++ lua/user/autocommands.lua
@@ -0,0 +1,5 @@
+-- Press any button to automatically update file content
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
