36c7fac39e477e3b00119bd3d3767bfec32001e9
Danil Shamsiev <veismahs@outlook.com>
2023-06-22 23:54:28 +0400

    style: switch to gruvsquirrel theme for more colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,20 +1,21 @@
 function SetColor(color)
-  color = color or "habamax"
+  color = color or "gruvsquirrel"
   vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "#151515" })
-  vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1c1c1c" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "#070707" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1a1a1a" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#151515" })
   vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#151515", fg = "#ffffff" })
   vim.api.nvim_set_hl(0, "FloatTitle", { bg = "#151515", fg = "#ffffff" })
-  vim.api.nvim_set_hl(0, "StatusLine", { fg = "#1c1c1c", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "none", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "LineNr", { fg = "#767676", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#767676", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#767676", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#eeeeee", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1c1c1c", bg = "#1c1c1c" })
-  vim.api.nvim_set_hl(0, "MsgSeparator", { fg = "#1c1c1c", bg = "#1c1c1c" })
+  vim.api.nvim_set_hl(0, "StatusLine", { fg = "#1a1a1a", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "none", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "LineNr", { fg = "#767676", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#767676", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#767676", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#eeeeee", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1a1a1a", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "MsgSeparator", { fg = "#1a1a1a", bg = "#1a1a1a" })
+  vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#1a1a1a" })
 end
 
 SetColor()
