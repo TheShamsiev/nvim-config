7130972c7620ffb9fb0e17a4b7da58e683190701
Danil Shamsiev <veismahs@outlook.com>
2023-06-28 00:24:42 +0400

    feat: winbar

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -58,4 +58,17 @@ return require("packer").startup(function(use)
   use("JoosepAlviste/nvim-ts-context-commentstring")
   use("goolord/alpha-nvim")
   use("mikesmithgh/gruvsquirrel.nvim")
+
+  use({
+    "utilyre/barbecue.nvim",
+    tag = "*",
+    requires = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    after = "nvim-web-devicons", -- keep this if you're using NvChad
+    config = function()
+      require("barbecue").setup()
+    end,
+  })
 end)
